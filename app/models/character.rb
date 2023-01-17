0bed57a4506d2449b186e032e792b9824affb053
Andrew Owens <andrrowens@gmail.com>
2023-01-16 19:00:57 -0700

    commit tree c53109f1ac27df4936cabeb2ada982c3fd05a764

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor # 7 methods
+  belongs_to :show
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def say_that_thing_you_say
+    "#{self.full_name} always says: #{self.catchphrase}"
+  end
+
 end
